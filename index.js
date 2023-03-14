41ffa2a1646d91101fc85e4fd496c8817a2a0bb8
danieljuma5 <jumadaniel275@gmail.com>
2023-03-14 12:02:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//Multiply
+let num1 = 31; 
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+//Random
+let random = Math.floor(Math.random() * 10) + 1;
+
+//Mod
+let num3 =30; 
+let num4= 13;
+let mod = num3 % num4;
+console.log(mod);
+
+//max
+let max = Math.max(15,16,17,18,19,20);
